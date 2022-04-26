3a76f10d6c920dd5f833fd7a2769a425f9760e7b
Savyasach Reddy E <190050109@iitb.ac.in>
2022-04-27 01:06:33 +0530

    Initiate React App

---
+++ client-interface/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from './components/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client-interface/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useState } from "react";
+import { useNavigate, Link } from 'react-router-dom';
+
+const Home = () => {
+
+
+  return (
+    <Fragment>
+      <div>
+        Hi
+      </div>
+    </Fragment >
+  );
+};
+
+export default Home;
