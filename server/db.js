d9419993a7349a1e6be9b7962523a09749f113ac
Savyasach Reddy E <190050109@iitb.ac.in>
2022-04-28 15:48:39 +0530

    Added to package.json

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "divijaved2002",
+  password: "postgres",
   host: "localhost",
   port: 5432,
   database: "project"
